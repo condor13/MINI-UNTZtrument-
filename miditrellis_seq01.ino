1131a2cc39f7130cab3fea4a5a2c8ce2f57f33c5
Teodor Nicolae <theonickolas@yahoo.com>
2018-12-19 12:32:26 +0200

    Update miditrellis_seq01.ino

--- miditrellis_seq01.ino
+++ miditrellis_seq01.ino
@@ -31,7 +31,14 @@ const uint8_t        addr[] =   { 0x70, 0x71, 0x72, 0x73,
 
 // Encoder on pins 4,5 sets tempo.  Optional, not present in
 // standard UNTZtrument, but won't affect things if unconnected.
-enc e(4, 5);
+
+byte MIDI_CLOCK = 0xf8;
+enc e[] = {
+  enc( 4, 5),
+  enc( 6, 7),
+  enc( 8, 9),
+  enc(10, 11)
+  enc(12, 13) };
 
 int velocity = 100;//velocity of MIDI notes, must be between 0 and 127
 int noteON = 144;//144 = 10010000 in binary, note on command
@@ -45,18 +52,23 @@ unsigned long beatInterval = 60000L / bpm, // ms/beat
               prevBeatTime = 0L,           // Column step timer
               prevReadTime = 0L;           // Keypad polling timer
 
+int16_t       nota1;
+int16_t       nota2;
+int16_t       nota3;
+int16_t       nota4;
+
 // The note[] and channel[] tables are the MIDI note and channel numbers
 // for to each row (top to bottom); they're specific to this application.
 // bitmask[] is for efficient reading/writing bits to the grid[] array.
 static const uint8_t PROGMEM
-  note[4]    = {  36, 37, 38, 39  },
+  note[4]    = {  nota1, nota2, nota3, nota4  },
   channel[4] = {   1,  1,  1,  1  },
   bitmask[4] = {   1,  2,  4,  8  };
 
 void setup() {
   Serial.begin(31250);
-  Serial.println("MIDI Demo");
-  Serial.println("V1");
+  Serial.println("MIDI-seq Demo-v0.2");
+  
   pinMode(LED, OUTPUT);
 #ifndef HELLA
   untztrument.begin(addr[0], addr[1]);
@@ -78,9 +90,20 @@ void setup() {
   untztrument.writeDisplay();
   memset(grid, 0, sizeof(grid));
   enc::begin();                     // Initialize all encoder pins
-  e.setBounds(60 * 4, 480 * 4 + 3); // Set tempo limits
-  e.setValue(bpm * 4);              // *4's for encoder detents
+  e[0].setBounds(60 * 4, 480 * 4 + 3); // Set tempo limits
+  e[1].setBounds(36, 80);           //
+  e[2].setBounds(36, 80);
+  e[3].setBounds(36, 80);
+  e[4].setBounds(36, 80);
+  
+  e[0].setValue(bpm * 4);              // *4's for encoder detents
+  e[1].getValue(36);
+  e[2].getValue(37);
+  e[3].getValue(38);
+  e[4].getValue(39);
+  
 }
+
 /*        MIDI USB CODE
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
@@ -97,9 +120,9 @@ void noteOff(byte channel, byte pitch, byte velocity) {
 
 //send MIDI message
 void MIDImessage(int command, int MIDInote, int MIDIvelocity) {
-  Serial.write(command);//send note on or note off command
-  Serial.write(MIDInote);//send pitch data
-  Serial.write(MIDIvelocity);//send velocity data
+  Serial.write(command);          //send note on or note off command
+  Serial.write(MIDInote);         //send pitch data
+  Serial.write(MIDIvelocity);     //send velocity data
 }
 
 // Turn on (or off) one column of the display
@@ -129,7 +152,7 @@ void loop() {
             grid[x] &= ~mask;
             untztrument.clrLED(i);
             MIDImessage(noteOFF, pgm_read_byte(&note[y]), 127); //turn note off
-            //noteOff(pgm_read_byte(&channel[y]), pgm_read_byte(&note[y]), 127);
+            //noteOff(pgm_read_byte(&channel[y]), pgm_read_byte(&note[y]), 127);     //This MIDIUSB
           } else { // Turn on
             grid[x] |= mask;
             untztrument.setLED(i);
@@ -163,7 +186,11 @@ void loop() {
     }
     prevBeatTime = t;
     refresh      = true;
-    bpm          = e.getValue() / 4; // Div for encoder detents
+    bpm          = e[0].getValue() / 4; // Div for encoder detents
+    nota1        = e[1].getValue();
+    nota2        = e[2].getValue();
+    nota3        = e[3].getValue();
+    nota4        = e[4].getValue();
     beatInterval = 60000L / bpm;
   }
 
